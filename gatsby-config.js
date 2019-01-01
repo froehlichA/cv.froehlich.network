34a9efec14d079d7d6a9bf65aab4d196037c8b48
Fröhlich A <froehlich.alexander@protonmail.com>
2019-01-01 21:12:14 +0100

    Add gatsby-image

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,17 @@
+const path = require('path');
+
 module.exports = {
-  plugins: [`gatsby-plugin-styled-components`]
+  plugins: [
+    `gatsby-plugin-styled-components`,
+    `gatsby-plugin-react-helmet`,
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: path.join(__dirname, `src`, `images`)
+      }
+    }
+  ]
 };
