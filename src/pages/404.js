f4d44ae5ac470e4928aab7275367c927da25da74
Alexander Fröhlich <froald14@new.edvhtl.at>
2019-02-27 08:10:21 +0100

    Add "funny" error messages

--- src/pages/404.js
+++ src/pages/404.js
@@ -4,13 +4,19 @@ import { Spacer } from '../components/layout';
 import { Hero, TextP } from '../components/typography';
 import { Link } from 'gatsby';
 
+const errorMessages = [
+  "Hmm... I don't know what went wrong, but something did.",
+  "Looks like one of us did something wrong.",
+  "Go back to start. Do not pass go, do not collect $200.",
+];
+
 export default () => (
   <Theme>
     <Spacer height="10rem" />
     <Hero>I'm sorry,</Hero>
     <Hero inverted>it's a 404</Hero>
     <Spacer height="1rem" />
-    <TextP>There aren't really any other pages. Maybe there will be more in the future?</TextP>
+    <TextP>{errorMessages[Math.floor(Math.random() * errorMessages.length)]}</TextP>
     <TextP>You can click <Link to="/">HERE</Link> to go back home.</TextP>
   </Theme>
-)
+);
