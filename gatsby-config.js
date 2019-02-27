bda527de29aca1dc82aaf0d7e1e393db865ca19f
Fröhlich A <froehlich.alexander@protonmail.com>
2019-02-27 09:16:01 +0100

    Improve readability of CV

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,7 +26,7 @@ module.exports = {
       resolve: `gatsby-mdx`,
       options: {
         defaultLayouts: {
-          default: require.resolve('./src/themes/default.js')
+          default: require.resolve('./src/themes/reading.js')
         }
       }
     }
