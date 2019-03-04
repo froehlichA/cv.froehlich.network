85d8485ef034812167ecc40fa79b6b8bb1f99118
Ouwel Zhang <ouwel.zhang@hotmail.com>
2019-03-04 09:23:02 +0100

    Add CV

--- src/themes/reading.js
+++ src/themes/reading.js
@@ -1,11 +1,15 @@
 import React from "react";
 import DefaultTheme from "./default";
 import { Reader } from "../components/layout";
+import { createGlobalStyle } from "styled-components";
+
+const GlobalReadingStyle = createGlobalStyle``;
 
 export default class ReadingTheme extends React.Component {
   render() {
     return (
       <DefaultTheme>
+        <GlobalReadingStyle />
         <Reader>{this.props.children}</Reader>
       </DefaultTheme>
     );
