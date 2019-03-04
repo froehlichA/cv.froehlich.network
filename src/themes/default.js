2fbed1cb5e1f458783e48e693e2103618aacee1d
Fröhlich A <froehlich.alexander@protonmail.com>
2019-03-04 10:24:51 +0100

    Fix Typo and remove theming

--- src/themes/default.js
+++ src/themes/default.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { Helmet } from "react-helmet";
-import { ThemeProvider } from "styled-components";
 
 import "../../node_modules/normalize.css/normalize.css";
 import "../../node_modules/flexboxgrid/css/flexboxgrid.css";
@@ -13,58 +12,32 @@ import { BoldLinkContainer, BoldLink } from "../components/typography/boldLink";
 import GithubCorner from "../components/github-corner";
 
 export default class DefaultTheme extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      theme: {
-        fg: "black",
-        lightfg: "grey",
-        lightbg: "lightgrey",
-        bg: "white"
-      }
-    };
-    this.invertTheme = this.invertTheme.bind(this);
-  }
-
-  invertTheme() {
-    this.setState(prevState => ({
-      theme: {
-        fg: prevState.theme.bg,
-        lightfg: prevState.theme.lightbg,
-        lightbg: prevState.theme.lightfg,
-        bg: prevState.theme.fg
-      }
-    }));
-  }
-
   render() {
     return (
-      <ThemeProvider theme={this.state.theme}>
-        <React.Fragment>
-          <Helmet>
-            <html lang="en" />
-            <title>Alexander Fröhlich</title>
-            <meta
-              name="Description"
-              content="Online Portfolio of Alexander Fröhlich"
-            />
-          </Helmet>
-          <GithubCorner />
-          <Leftbar>
-            <LeftbarItem>A</LeftbarItem>
-            <LeftbarItem>F</LeftbarItem>
-          </Leftbar>
-          <Background>
-            <Container>
-              <BoldLinkContainer>
-                <BoldLink to="/">Home</BoldLink> /{" "}
-                <BoldLink to="/cv">CV</BoldLink>
-              </BoldLinkContainer>
-              {this.props.children}
-            </Container>
-          </Background>
-        </React.Fragment>
-      </ThemeProvider>
+      <React.Fragment>
+        <Helmet>
+          <html lang="en" />
+          <title>Alexander Fröhlich</title>
+          <meta
+            name="Description"
+            content="Online Portfolio of Alexander Fröhlich"
+          />
+        </Helmet>
+        <GithubCorner />
+        <Leftbar>
+          <LeftbarItem>A</LeftbarItem>
+          <LeftbarItem>F</LeftbarItem>
+        </Leftbar>
+        <Background>
+          <Container>
+            <BoldLinkContainer>
+              <BoldLink to="/">Home</BoldLink> /{" "}
+              <BoldLink to="/cv">CV</BoldLink>
+            </BoldLinkContainer>
+            {this.props.children}
+          </Container>
+        </Background>
+      </React.Fragment>
     );
   }
 }
